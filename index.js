d156e1e7602ecee9d8b7778d459b41f6f6c80a77
Rudo Kemper <31662219+rudokemper@users.noreply.github.com>
2022-11-08 19:52:29 -0500

    add content-encoding header for gzipped tiles

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.use(express.static('.'));
+app.use(express.static('.', {
+    // Set content-encoding header for gzipped vector tiles produced for use with Mapeo, which end in .vector.pbf
+    setHeaders: (res, url) => {
+        if (url.endsWith('.vector.pbf')) {
+         res.setHeader('content-encoding', 'gzip');
+        }
+}}));
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
