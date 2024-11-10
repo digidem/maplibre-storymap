7bc4b8ec8a3bf7d8dc6c7ac43b0bf414b4b48977
Rudo Kemper <rtakemper@gmail.com>
2024-11-09 19:22:21 -0500

    Allow setting a custom port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.argv[2] || 5000;
 
 app.use(express.static('.', {
-    // Set content-encoding header for gzipped vector tiles produced for use with Mapeo, which end in .vector.pbf
+    // Set content-encoding header for gzipped vector tiles
     setHeaders: (res, url) => {
         if (url.endsWith('.vector.pbf')) {
          res.setHeader('content-encoding', 'gzip');
@@ -11,6 +11,10 @@ app.use(express.static('.', {
     }
 }));
 
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+app.listen(port, (err) => {
+    if (err) {
+        console.error('Failed to start server:', err);
+    } else {
+        console.log(`Now listening on port ${port}`);
+    }
 });
